02c2bcb6bfed44ecccfee5bb790b224b85f870d6
SHiroaki <hiroaki_suzuki@jaist.ac.jp>
2014-08-11 05:05:42 +0900

    2014/8/11

--- GA/genetic_methods.py
+++ GA/genetic_methods.py
@@ -6,6 +6,15 @@ import numpy as np
 import bitstring
 from matplotlib import pyplot as plt
 
+def decode_gtype(binary_string):
+    """gtype -> ptype"""
+    
+    #bitのリストを文字列に変換
+    ind_string = "0b" + "".join(map(str, binary_string)) 
+    bitobj = bitstring.BitArray(ind_string)
+    ptype_value = bitobj.uint  #符号なしで扱うこと
+    return ptype_value 
+
 def make_indviduals(INT_MIN, INT_MAX):
     """ランダムな整数を得る(グレイコードに変換はあとで)"""
     """[bitstring.obj, bitstring.obj, ....]を返す. bitstring.obj.int
@@ -14,9 +23,26 @@ def make_indviduals(INT_MIN, INT_MAX):
     random.seed() #乱数生成器を初期化
     ivalue = random.randint(INT_MIN, INT_MAX)
     binaryobj = bitstring.BitArray(uint=ivalue, length=10)  #ここも符号なしで
-    ind_binary = (int(x) for x in binaryobj.bin)
+    graycode = binaryobj ^ (binaryobj >> 1) #grayコードに変換
+    #print ivalue, graycode.bin, binaryobj.bin
+    ind_binary = (int(x) for x in graycode.bin)
     
     return ind_binary
+
+def decode_gray(gray_string):
+    """グレイコード(list)を受け取り２進数(list)に変換する
+    """
+    binary_list = []
+    binary_list.append(gray_string[0]) #先頭bitはそのまま
+
+    for i, x in enumerate(gray_string[1:]):
+        b = x ^ binary_list[i]
+        binary_list.append(b)
+
+    #print binary_list ok
+    return binary_list
+
+
 
 def evaluate(ind):
     """評価関数
@@ -26,10 +52,7 @@ def evaluate(ind):
     sigma = 25000.0
     myu = 500.0
     
-    ind_string = "0b" + "".join(map(str,ind)) #bitのリストを文字列に変換
-    bitobj = bitstring.BitArray(ind_string)
-    uint_value = bitobj.uint  #符号なしで扱うこと
-    
+    uint_value = decode_gtype(ind)
     #正規分布の確率密度関数に代入
     #型を揃えてから数式は計算しよう
     normal_dist_bias = ((1.0/np.sqrt(2*np.pi*sigma)) * 
@@ -44,7 +67,8 @@ if __name__ == "__main__":
     ylist = []
     sigma = 25000
     myu = 500
-    
+
+    #exit()
     f = open("res.txt","w")
     for x in np.arange(0., 1000., 1):
         xlist.append(x)
--- GA/genetic_scurve.py
+++ GA/genetic_scurve.py
@@ -20,11 +20,11 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 
 #Initialization
 IND_SIZE = 1
-INT_MIN = 0
-INT_MAX = 100
+INT_MIN = 200
+INT_MAX = 250
 toolbox = base.Toolbox()
 toolbox.register("attribute", genetic_methods.make_indviduals, 
-                 INT_MIN, INT_MAX) #0~150の間の整数
+                 INT_MIN, INT_MAX) 
 toolbox.register("individual",tools.initIterate, creator.Individual,
                  toolbox.attribute) 
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -33,9 +33,9 @@ pool = multiprocessing.Pool()
 toolbox.register("map", pool.map)  #並列化.効あり
 
 #log setting
-logbook_header = ["avg", "std", "min", "max", "bestind"]
-logbook_func = [np.mean, np.std, np.min, np.max, tools.selBest]
-data_segment = [0,0,0,0,1] #recordでわたすデータの何番目のリストを使うか指定 
+logbook_header = ["avg","bestind", "avgind"]
+logbook_func = [np.mean, tools.selBest, np.mean]
+data_segment = [0,1,2] #recordでわたすデータの何番目のリストを使うか指定 
 logbook = LogBook(logbook_header, logbook_func, data_segment)
 
 #Evaluate function
@@ -43,7 +43,8 @@ logbook = LogBook(logbook_header, logbook_func, data_segment)
 #Operators
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=0.05) #10bit中1bitが変化する
-toolbox.register("select", tools.selTournament, tournsize=2)
+toolbox.register("select", tools.selTournament, tournsize=5)
+#toolbox.register("select", tools.selRandom)
 #評価関数も必ずタプルで返す。中身はfloatにする
 toolbox.register("evaluate", genetic_methods.evaluate)  
 
@@ -51,14 +52,17 @@ toolbox.register("evaluate", genetic_methods.evaluate)
 def main():
     """Complete generational algorithm
     mapを使うとジェネレータが帰ってくる"""
-    n=10
+
+    n=100
     pop = toolbox.population(n)
     #交叉も突然変異もしてないのに個体が変わる
     #ベストも変わる
-    CXPB, MUTPB, NGEN = 0.6, 0.01, 100
+    CXPB, MUTPB, NGEN = 0.6, 0.002, 500 
+    #結構突然変異起こる. 起こらないと値は揺れない
 
     #初期個体の評価
     fitnesses = toolbox.map(toolbox.evaluate, pop) #[(6782,),(2342,)...]になってる
+
     
     for ind, fit in zip(pop, fitnesses): #ここでfitnessesが遅延評価される
         #print ind, fit
@@ -66,24 +70,18 @@ def main():
 
     #Select the next generation individuals
     for g in xrange(NGEN):
+        random.seed()
         #print "------------" + str(g) + "------------"
         #g世代の個体についてstatisticsで設定した値を記録
         logbook.add_dictionary(g)
         #すべての個体の適応度をリストにまとめる
         fits = [ind.fitness.values for ind in pop]
-        b2 = tools.selBest(sorted(pop), 1)
-        bi = "0b"+"".join(toolbox.map(str, b2[0]))
-        b = bitstring.BitArray(bi)
-        print "individuals"
-        for x in pop:
-            bit = "0b" + "".join(toolbox.map(str, x))
-            bito = bitstring.BitArray(bit)
-            print bito.uint, x.fitness.values[0],toolbox.evaluate(bit),
-        print ":: best", b.uint, toolbox.evaluate(b2[0])
+        pop_ptype = [genetic_methods.decode_gtype(x) for x in pop]
+        
         #fits_tup = [(ind, ind.fitness.values) for ind in pop]
-        # recordに渡す値は実行する関数と対応
-        #関数の引数が複数の場合は(データ、引数)で渡す
-        logbook.record(fits, (pop, 1))
+        # recordに渡す値は実行する関数と対応.適応度以外はすべてタプルで
+        #関数の引数が複数の場合は(データ、引数)で渡す.
+        logbook.record(fits, (pop, 1), (pop_ptype,))
 
         offspring = toolbox.select(pop, len(pop))
 
@@ -101,8 +99,10 @@ def main():
                 del child2.fitness.values
 
         for mutant in offspring:
-            if random.random() < MUTPB:
+            a = random.random()
+            if a < MUTPB:
                 toolbox.mutate(mutant)
+                #print a, "mutate : %d" % g
                 del mutant.fitness.values
 
         #Evaluate the individuals with an invalid fitness
@@ -117,7 +117,6 @@ def main():
         #The population is entirely replaced by the offspring
         #pop = toolbox.select(pop + offspring, len(offspring))
         pop[:] = offspring
-        
 
     return logbook
 
@@ -126,35 +125,33 @@ if __name__ == "__main__":
     
     #Plotting graph
     gen = logdata.select("gen")
-    fit_mins = logdata.select("min")
+    #fit_mins = logdata.select("min")
     fit_avgs = logdata.select("avg")
-    fit_max = logdata.select("max")
+    ptype_avgs = logdata.select("avgind")
+    #fit_max = logdata.select("max")
     best_ind = logdata.select("bestind")
 
-    v = []
-    for x in best_ind:
-        bit = "0b" + "".join(toolbox.map(str, x[0]))
-        bito = bitstring.BitArray(bit)
-        #print bito.uint
-        v.append(bito.uint)
+    #print ptype_avgs
+    v = [genetic_methods.decode_gtype(x[0]) for x in best_ind]
 
-    
     fig, ax1 = plt.subplots()
     line1 = ax1.plot(gen, v, "b-", label="Best Individual")
     ax1.set_xlabel("Generation")
-    ax1.set_ylabel("Best Individual", color="b")
+    ax1.set_ylabel("PTYPE VALUE")
+
+    line2 = ax1.plot(gen, ptype_avgs,"g-", label="PTYPE Mean")
 
     for tl in ax1.get_yticklabels():
-        tl.set_color("b")
+        tl.set_color("black")
 
     ax2 = ax1.twinx()
-    line2 = ax2.plot(gen, fit_avgs, "r-", label="Average Fitness")
+    line3 = ax2.plot(gen, fit_avgs, "r-", label="Average Fitness")
     ax2.set_ylabel("Average Fitness", color="r")
 
     for tl in ax2.get_yticklabels():
         tl.set_color("r")
 
-    lns = line1 + line2
+    lns = line1 + line2 + line3
     labs = [l.get_label() for l in lns]
     ax1.legend(lns, labs, loc="center right")
     plt.show()
